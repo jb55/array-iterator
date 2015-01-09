87b4cca7adcdf68d1b63fe164fd92939a45f16c5
Alexey Raspopov <avenger7x13@gmail.com>
2015-01-09 19:58:55 +0200

    fallback to yield

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-
-module.exports = function arrayIter(arr) {
+function arrayIterNative(arr) {
   return arr[Symbol.iterator]();
 }
+
+function* arrayIter(arr) {
+  for (var i = 0, len = arr.length; i < len; i++) yield arr[i];
+}
+
+module.exports = typeof Symbol === 'undefined' || typeof Symbol.iterator === 'undefined' ? arrayIter : arrayIterNative;
